65e3b8788fb4b847ac1421747ced9a4ea1ea5523
Asyraf Duyshart <me@asyraf.id>
2020-02-21 17:01:43 +0700

    - add multiple proxy

--- server.go
+++ server.go
@@ -3,10 +3,8 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 	"time"
 
@@ -15,135 +13,16 @@ import (
 	log "github.com/asyrafduyshart/go-reverse-proxy/log"
 )
 
-// Server struct file
-type Server struct {
-	Name           string              `yaml:"name"`
-	Listen         string              `yaml:"listen"`
-	Domains        []string            `yaml:"domains"`
-	Root           *string             `yaml:"root"`
-	SSL            bool                `yaml:"ssl"`
-	GZIP           bool                `yaml:"gzip"`
-	GFW            bool                `yaml:"gfw"`
+// Proxy struct file
+type Proxy struct {
 	ProxyPass      *string             `yaml:"proxy_pass"`
 	ProxyPath      *string             `yaml:"proxy_path"`
 	RequestHeaders []map[string]string `yaml:"request_headers"`
-	KeyFile        string              `yaml:"key_file"`
-	CertFile       string              `yaml:"cert_file"`
 }
 
-// Keys Return keys of the given map
-func Keys(m map[string]string) map[string]interface{} {
-	po := make(map[string]interface{})
-	for k := range m {
-		po[k] = m[k]
-	}
-	return po
-}
+func (p Proxy) setup(w http.ResponseWriter, r *http.Request) {
 
-// Start the server
-func (s *Server) Start() {
-	if s.ProxyPass != nil {
-		log.Info("%s listen %s, ssl: %v, proxy to %s", s.Name, s.Listen, s.SSL, *s.ProxyPass)
-	} else {
-		if s.Root != nil {
-			log.Info("%s listen %s, ssl: %v, static dir %s", s.Name, s.Listen, s.SSL, *s.Root)
-		}
-	}
-
-	r := mux.NewRouter()
-
-	api := r.PathPrefix(*s.ProxyPath).Subrouter()
-	api.HandleFunc("", s.fuckGFW)
-	api.HandleFunc("/", s.fuckGFW)
-	api.HandleFunc(`/{rest:[a-zA-Z0-9=\-\/]+}`, s.fuckGFW)
-
-	if s.Root != nil {
-		pathLocation := *s.Root
-		log.Info("Config location %s", pathLocation)
-		r.PathPrefix("/").Handler(http.FileServer(http.Dir("." + pathLocation)))
-	}
-
-	var err error
-	if s.SSL {
-		err = http.ListenAndServeTLS(s.Listen, s.CertFile, s.KeyFile, r)
-	} else {
-		err = http.ListenAndServe(s.Listen, r)
-	}
-
-	if err != nil {
-		log.Error("%v", err)
-	}
-}
-
-// Proxy server
-func (s *Server) Proxy(w http.ResponseWriter, r *http.Request) {
-	realurl := *s.ProxyPass + r.RequestURI
-	log.Info("RealURL: %s", realurl)
-
-	o := new(http.Request)
-
-	*o = *r
-	targetURL, err := url.Parse(*s.ProxyPass)
-	log.Info("Target Url: %s", targetURL)
-
-	o.Host = targetURL.Host
-	o.URL.Scheme = targetURL.Scheme
-	o.URL.Host = targetURL.Host
-	o.URL.Path = singleJoiningSlash(targetURL.Path, o.URL.Path)
-
-	if q := o.URL.RawQuery; q != "" {
-		o.URL.RawPath = o.URL.Path + "?" + q
-	} else {
-		o.URL.RawPath = o.URL.Path
-	}
-
-	o.URL.RawQuery = targetURL.RawQuery
-
-	o.Proto = "HTTP/1.1"
-	o.ProtoMajor = 1
-	o.ProtoMinor = 1
-	o.Close = false
-
-	transport := http.DefaultTransport
-	res, err := transport.RoundTrip(o)
-
-	if err != nil {
-		log.Error("http: proxy error: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	hdr := w.Header()
-
-	for k, vv := range res.Header {
-		for _, v := range vv {
-			hdr.Add(k, v)
-		}
-	}
-
-	for _, c := range res.Cookies() {
-		hdr.Add("Set-Cookie", c.Raw)
-	}
-
-	if s.GZIP {
-		hdr.Add("Content-Encoding", "gzip")
-	}
-
-	w.WriteHeader(res.StatusCode)
-
-	if res.Body != nil {
-		io.Copy(w, res.Body)
-	}
-
-}
-
-var transport = &http.Transport{
-	ResponseHeaderTimeout: 30 * time.Second,
-	TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
-}
-
-func (s *Server) fuckGFW(w http.ResponseWriter, r *http.Request) {
-	realurl := *s.ProxyPass + r.RequestURI
+	realurl := *p.ProxyPass + r.RequestURI
 	log.Info("RealURL: %s", realurl)
 
 	req, err := http.NewRequest(r.Method, realurl, r.Body)
@@ -155,16 +34,8 @@ func (s *Server) fuckGFW(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// req.Header.Set("Accept", r.Header.Get("Accept"))
-	// req.Header.Set("Accept-Encoding", r.Header.Get("Accept-Encoding"))
-	// req.Header.Set("Accept-Language", r.Header.Get("Accept-Language"))
-	// req.Header.Set("Cache-Control", "no-cache")
-	// req.Header.Set("Pragma", "no-cache")
-	// req.Header.Set("User-Agent", r.Header.Get("User-Agent"))
-	// req.Header.Set("Cookie", r.Header.Get("Cookie"))
-
-	for k := range s.RequestHeaders {
-		headerReq := Keys(s.RequestHeaders[k])
+	for k := range p.RequestHeaders {
+		headerReq := Keys(p.RequestHeaders[k])
 		for j := range headerReq {
 			log.Info("Header: %s | Value: %s", j, headerReq[j])
 			str := fmt.Sprintf("%v", headerReq[j])
@@ -214,7 +85,73 @@ func (s *Server) fuckGFW(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 	w.WriteHeader(resp.StatusCode)
 	w.Write(data)
+}
 
+// Server struct file
+type Server struct {
+	Name     string   `yaml:"name"`
+	Listen   string   `yaml:"listen"`
+	Domains  []string `yaml:"domains"`
+	Root     *string  `yaml:"root"`
+	SSL      bool     `yaml:"ssl"`
+	GZIP     bool     `yaml:"gzip"`
+	GFW      bool     `yaml:"gfw"`
+	Proxies  []Proxy  `yaml:"proxies"`
+	KeyFile  string   `yaml:"key_file"`
+	CertFile string   `yaml:"cert_file"`
+}
+
+// Keys Return keys of the given map
+func Keys(m map[string]string) map[string]interface{} {
+	po := make(map[string]interface{})
+	for k := range m {
+		po[k] = m[k]
+	}
+	return po
+}
+
+// Start the server
+func (s *Server) Start() {
+
+	if s.Root != nil {
+		log.Info("%s listen %s, ssl: %v, static dir %s", s.Name, s.Listen, s.SSL, *s.Root)
+	}
+
+	r := mux.NewRouter()
+	for _, proxy := range s.Proxies {
+		if proxy.ProxyPass != nil {
+			log.Info("%s listen %s, ssl: %v, proxy to %s", s.Name, s.Listen, s.SSL, *proxy.ProxyPass)
+		}
+
+		fmt.Println("PROXY", *proxy.ProxyPath)
+
+		r.PathPrefix(*proxy.ProxyPath).Subrouter()
+		r.PathPrefix(*proxy.ProxyPath).Subrouter().HandleFunc("", proxy.setup)
+		r.PathPrefix(*proxy.ProxyPath).Subrouter().HandleFunc("/", proxy.setup)
+		r.PathPrefix(*proxy.ProxyPath).Subrouter().HandleFunc(`/{rest:[a-zA-Z0-9=\-\/]+}`, proxy.setup)
+	}
+
+	if s.Root != nil {
+		pathLocation := *s.Root
+		log.Info("Config location %s", pathLocation)
+		r.PathPrefix("/").Handler(http.FileServer(http.Dir("." + pathLocation)))
+	}
+
+	var err error
+	if s.SSL {
+		err = http.ListenAndServeTLS(s.Listen, s.CertFile, s.KeyFile, r)
+	} else {
+		err = http.ListenAndServe(s.Listen, r)
+	}
+
+	if err != nil {
+		log.Error("%v", err)
+	}
+}
+
+var transport = &http.Transport{
+	ResponseHeaderTimeout: 30 * time.Second,
+	TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
 }
 
 func singleJoiningSlash(a, b string) string {
